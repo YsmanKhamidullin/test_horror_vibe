d6bd07cfda76541eb2a37184791f2ef2a4d7197e
YsmanKhamidullin <YsmanKhamidullin@gmail.com>
2025-05-28 03:04:06 +0500

    upd textures for webgl, change ending text

--- Assets/Game/Core/Canvas/Base/TrueEnding.cs
+++ Assets/Game/Core/Canvas/Base/TrueEnding.cs
@@ -15,11 +15,11 @@ namespace Game.Core.Canvas.Base
         public async UniTask Animate()
         {
             _canvasGroup.alpha = 0f;
-            _label.SetText("Game by: Ysman Khamidullin");
+            _label.SetText("Game by: TsukiGan");
             await _canvasGroup.DOFade(1f, 1.5f).SetEase(Ease.OutSine).ToUniTask();
             await UniTask.WaitForSeconds(0.2f);
             await _label.Show();
-            await UniTask.WaitForSeconds(1f);
+            await UniTask.WaitForSeconds(2f);
 
             _label.SetText("Thank you for playing!");
             await _label.Show();
